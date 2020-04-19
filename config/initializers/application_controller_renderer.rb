e911a305b1eb12b71b8f224106e62852baf54cf1
nakagawanatsu <nakagawa.natsu.infratop@gmail.com>
2020-04-19 23:07:21 +0900

    refinish

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '1e5ef283e822f7ff3d3c9379d6e40f497b5b94cc6859541f39a759b70c8aebac347b2c0c21c92a21223b478c17bc91fb9c89ab8c2abe858cbc0f944ba905cd22'
